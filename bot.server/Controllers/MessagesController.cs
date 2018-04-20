a7030d76ec186480fc83dff8caadff84f8556f19
gplesz <gabor.plesz@gmail.com>
2018-04-20 16:54:37 +0200

    #2: első működő csetrobot: visszhang

--- bot.server/Controllers/MessagesController.cs
+++ bot.server/Controllers/MessagesController.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Connector;
 
 namespace bot.server.Controllers
 {
     [Route("api/[controller]")]
     public class MessagesController : Controller
     {
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpPost]
+        public async Task<OkResult> Post([FromBody]Activity activity)
         {
-            return "value";
+            var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            var reply = activity.CreateReply($"ezt kaptam: {activity.Text}");
+            await connector.Conversations.ReplyToActivityAsync(reply);
+            return Ok();
         }
-
     }
 }
